3e1492be2153bf501f013fde28d4b9f1495683d6
Mehmet <sumer682000@gmail.com>
2022-03-14 12:10:40 +0300

    Initial commit

---
+++ pages/pokemon/[id].js
@@ -0,0 +1,80 @@
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable @next/next/no-img-element */
+
+import Head from "next/head";
+import Link from "next/link";
+import styles from "../../styles/Details.module.css";
+
+import React from "react";
+
+export async function getStaticPaths() {
+  const resp = await fetch(
+    "https://jherr-pokemon.s3.us-west-1.amazonaws.com/index.json"
+  );
+
+  const pokemon = await resp.json();
+
+  return {
+    paths: pokemon.map((pok) => ({
+      params: { id: pok.id.toString() },
+    })),
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params }) {
+  const resp = await fetch(
+    `https://jherr-pokemon.s3.us-west-1.amazonaws.com/pokemon/${params.id}.json`
+  );
+
+  return {
+    props: {
+      pokemon: await resp.json(),
+    },
+  };
+}
+
+export default function Details({ pokemon }) {
+  if (!pokemon) return null;
+
+  return (
+    <div>
+      <Head>
+        <title>{pokemon.name}</title>
+      </Head>
+      <div>
+        <Link href="/">
+          <a>Back to Home</a>
+        </Link>
+      </div>
+      <div className={styles.layout}>
+        <div>
+          <img
+            className={styles.picture}
+            src={`https://jherr-pokemon.s3.us-west-1.amazonaws.com/${pokemon.image}`}
+          ></img>
+        </div>
+      </div>
+      <div>
+        <div className={styles.name}>{pokemon.name}</div>
+        <div className={styles.type}>{pokemon.type.join(", ")}</div>
+        <table>
+          <thead className={styles.header}>
+            <tr>
+              <th>Name</th>
+              <th>Value</th>
+            </tr>
+          </thead>
+          <tbody>
+            {pokemon.stats.map(({ name, value }) => (
+              <tr key={name}>
+                <td className={styles.attribute}>{name}</td>
+                <td>{value}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+}
